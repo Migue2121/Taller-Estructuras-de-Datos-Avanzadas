aa5522125de2c674b8d5d64537be43875745597a
Migue2121 <116528826+Migue2121@users.noreply.github.com>
2022-12-06 21:34:22 -0500

    Update Main2.java

--- Problema5/src/Main2.java
+++ Problema5/src/Main2.java
@@ -8,7 +8,7 @@ public class Main2 {
      public static final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
         List<Integer> path = search(original, target);
         TreeNode res = cloned;
-        // use the same path to walk through the cloned tree
+        
         for (int i = path.size() - 1; i >= 0; i--) {
             int d = path.get(i);
             if (d == 1) {
@@ -20,7 +20,7 @@ public class Main2 {
         return res;
     }
     
-    // search for the path from root to target, -1 for turn left, 1 for turn right, 0 for no turn,  return null if no valid path
+   
     private static List<Integer> search(TreeNode root, TreeNode t) {
         if (root == null) return null;
         List<Integer> res = new ArrayList<>();
